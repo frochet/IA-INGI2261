34586c89a9e6ec448d92091869a42ad1f66d8d7e
Florentin Rochet <oversnals@hotmail.com>
2012-09-24 16:33:04 +0200

    Ajout Assignement1

---
+++ Assignement1/test.py
@@ -0,0 +1,18 @@
+'''
+Created on 24 sept. 2012
+
+@author: Florentin
+'''
+from search import *
+
+class Test:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
