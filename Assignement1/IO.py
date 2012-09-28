a206def1be05f0f03e946c664efa8237c04ee3e6
Florentin Rochet <oversnals@hotmail.com>
2012-09-28 12:01:47 +0200

    IO

--- Assignement1/IO.py
+++ Assignement1/IO.py
@@ -6,7 +6,29 @@ Created on 28 sept. 2012
 class IO:
     
     def __init__(self,path) :
-        self.path = path
+        self.path = path 
+          
+
+    def init_reader(self):
+        """
+        Initialize the reader. You must do it before to use any method to read in the file
+        """
+        self.file = open(self.path,"r")
+    
+    def read_line(self):
+        """
+        You can use too "for line in IO.file : ..." after having called init_reader
+        """
+        return self.file.readline()
+    
+    
+    def close(self):
+        self.file.close()
         
-    def readLine(self):
-        pass
+# working if test.txt exist.
+if __name__ == "__main__":
+    Io = IO("test.txt")
+    Io.init_reader()
+    print(Io.read_line())
+    Io.close()
+    
