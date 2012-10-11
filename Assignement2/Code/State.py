64600b34465555b6d3c73c189d6a221b48cc5550
Florentin Rochet <oversnals@hotmail.com>
2012-10-11 14:42:14 +0200

    add state

---
+++ Assignement2/Code/State.py
@@ -0,0 +1,19 @@
+'''
+Created on 11 oct. 2012
+
+@author: Florentin
+'''
+
+class State:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+        
