9e861ea5a4159b95260186ac8a89c08911591bfe
Florentin Rochet <oversnals@hotmail.com>
2012-10-11 11:22:40 +0200

    Add IO, Case and Direction

---
+++ Assignement2/Code/Case.py
@@ -0,0 +1,14 @@
+'''
+Created on 11 oct. 2012
+
+@author: Florentin
+'''
+
+class Case:
+    '''
+        Enumration that represent a case on the board.
+    '''
+    GOAL = 0
+    NORMAL = 1
+    WALL = 2
+    STATIC_DEAD_STATE = 3
