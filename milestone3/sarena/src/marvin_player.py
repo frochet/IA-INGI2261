a9716a7f99d0c9a0067b313fc3e500cf05d246df
Florentin Rochet <oversnals@hotmail.com>
2012-11-10 22:34:51 +0100

    evaluate working and Action working

--- milestone3/sarena/src/marvin_player.py
+++ milestone3/sarena/src/marvin_player.py
@@ -21,19 +21,86 @@ class Marvin_player(Player,minimax.Game):
         self.mustDoDic = dict()
         self.suicideDic = dict()
         self.count_played = 0 # In order to know how many time we have played.
+        self._init_pattern()
         # remplir les dict de pattern TODO
     
     def successors(self, state):
         board, player = state
+        #temporaly
+        for action in board.get_actions():
+            yield (action,(board.clone().play_action(action), -player)) 
         # TODO
 
     def cutoff(self, state, depth):
         board, player = state
         # TODO
+        return depth == 5
 
     def evaluate(self, state):
         board, player = state
-        # TODO
+        #regarder les points en cours sur le board (jeton isoles + tour a retourner + pattern must_do, suicide) + la difference entre le nombre de jeton
+        # down de nos tours avec ceux de l'ennemi.
+        #return board.get_score()
+        #print("test")
+        score = 0
+        yellowCoins = 0
+        redCoins = 0
+        for i,j,tower in board.get_towers() :
+            towerHeight = board.get_height(tower) # in number of coins
+            if towerHeight > 0 :
+                isIsolated = True
+                for action in board.get_tower_actions(i,j):
+                    # Verify if a pattern exist and compute points.
+                    isIsolated = False
+                    k,l,m,n = action
+                    t_init = list()
+                    t_target = list()
+                    t_init+=board.m[k][l]
+                    t_target+=board.m[m][n]
+                    move = Action(t_init,t_target)
+                    if player == 1 : # si c'est a nous de jouer ?
+                        if move.is_a_pattern(self.mustDoDic) :
+                            score+=move.weight
+                    else:
+                        if move.is_a_pattern(self.suicideDic):
+                            score+=move.weight    
+                #compute points on the current towers    
+                print(str(tower))
+                if tower[0] == 3 :
+                    # look for a isolated tower :
+                    isIsolated = False
+                    newScore = self._see_if_isolate(i,j,board,score,tower)
+                    if newScore != score : 
+                        isIsolated = True
+                        score = newScore
+                     
+                    if not isIsolated:
+                        if tower[1][0] == Color.YELLOW :
+                            yellowCoins += 1
+                        elif tower[1][0] == Color.RED :
+                            redCoins += 1
+                elif tower[0] == 4 :
+                    if isIsolated :
+                        if tower[towerHeight][1] == Color.YELLOW :
+                            score += towerHeight
+                        elif tower[towerHeight][1] == Color.RED :
+                            score -= towerHeight
+                    else :
+                        if tower[4][1] == Color.YELLOW :
+                            score += 4 # 1 for 1 coin win
+                        elif tower[4][1] == Color.RED :
+                            score -= 4
+                        if tower[towerHeight][1] == Color.YELLOW :
+                            yellowCoins+=1
+                        elif tower[towerHeight][1] == Color.RED :
+                            redCoins +=1
+                print("end of compute fo current")
+        score += (yellowCoins-redCoins)
+        return score
+            
+            
+            
+                
 
     def play(self, percepts, step, time_left):
         if step % 2 == 0:
@@ -45,49 +112,97 @@ class Marvin_player(Player,minimax.Game):
         state = (Board(percepts), player)
         return minimax.search(state, self)  
     
+    def _see_if_isolate(self,i,j,board,score,tower):
+        def compute(score,tower):
+            if tower[1][0] == Color.YELLOW :
+                score += board.get_hight(tower)
+            elif tower[1][0] == Color.RED :
+                score -= board.get_hight(tower)
+            return score
+        i_start = 0
+        j_start = 0
+        i_end = len(board.m)-1
+        j_end = len(board.m[0])-1
+        if (i > i_start and i < i_end) and (j > j_start and j < j_end) :
+            if board.get_height(board.m[i][j+1])+board.get_height(board.m[i][j-1]) + \
+                board.get_height(board.m[i+1][j])+ board.get_height(board.m[i-1][j]) == 0 :
+                return compute(score,tower)
+        elif i==i_start and j==j_start:
+            if board.get_height(board.m[i][j+1]) + board.get_height(board.m[i+1][j]) == 0 :
+                return compute(score,tower)
+        elif i==i_end and j == j_start:
+            if board.get_height(board.m[i][j+1]) + board.get_height(board.m[i-1][j]) == 0 :
+                return compute(score,tower)
+        elif i==i_start and j == j_end:
+            if board.get_height(board.m[i+1][j]) + board.get_height(board.m[i-1][j]) == 0 :
+                return compute(score,tower)  
+        elif i==i_end and j == j_end:
+            if board.get_height(board.m[i-1][j]) + board.get_height(board.m[i][j-1]) == 0 :
+                return compute(score,tower) 
+        elif i==i_start and (j > j_start and j < j_end) :
+            if board.get_height(board.m[i][j+1]) +board.get_height(board.m[i][j-1])+ \
+                board.get_height(board.m[i+1][j]) == 0:
+                return compute(score,tower)
+        elif i==i_end and (j > j_start and j < j_end) :
+            if board.get_height(board.m[i][j+1]) +board.get_height(board.m[i][j-1])+ \
+                board.get_height(board.m[i-1][j]) == 0:
+                return compute(score,tower)
+        elif (i > i_start and i < i_end) and j==j_start :
+            if board.get_height(board.m[i+1][j]) +board.get_height(board.m[i-1][j])+ \
+                board.get_height(board.m[i][j+1]) == 0:
+                return compute(score,tower)
+        elif (i > i_start and i < i_end) and j==i_end :
+            if board.get_height(board.m[i+1][j]) +board.get_height(board.m[i-1][j])+ \
+                board.get_height(board.m[i][j-1]) == 0:
+                return compute(score,tower)      
+        return score
+        
     def _init_pattern(self):
         # Pattern Sandwich 
         self.mustDoDic[Action([3,[Color.RED, -2],[0,0],[0,0],[0,0]],[4,[-2,Color.RED],[0,0],[0,0],[0,0]])] = True
-        self.mustDoDic[Action([3,[Color.RED, -2],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,Color.RED,][0,0],[0,0]])] = True
+        self.mustDoDic[Action([3,[Color.RED, -2],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,Color.RED],[0,0],[0,0]])] = True
         self.mustDoDic[Action([3,[Color.RED, -2],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[-2,Color.RED],[0,0]])] = True
         self.mustDoDic[Action([3,[Color.RED, -2],[-2,-2],[0,0],[0,0]],[4,[-2,Color.RED],[0,0],[0,0],[0,0]])] = True
         self.mustDoDic[Action([3,[Color.RED, -2],[-2,-2],[-2,-2],[0,0]],[4,[-2,Color.RED],[0,0],[0,0],[0,0]])] = True
         self.mustDoDic[Action([3,[Color.RED, -2],[-2,-2],[0,0],[0,0]],[4,[-2,-2],[-2,Color.RED],[0,0],[0,0]])] = True
         
         self.suicideDic[Action([3,[Color.YELLOW, -2],[0,0],[0,0],[0,0]],[4,[-2,Color.YELLOW],[0,0],[0,0],[0,0]])] = True
-        self.suicideDic[Action([3,[Color.YELLOW, -2],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,Color.YELLOW,][0,0],[0,0]])] = True
+        self.suicideDic[Action([3,[Color.YELLOW, -2],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,Color.YELLOW],[0,0],[0,0]])] = True
         self.suicideDic[Action([3,[Color.YELLOW, -2],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[-2,Color.YELLOW],[0,0]])] = True
         self.suicideDic[Action([3,[Color.YELLOW, -2],[-2,-2],[0,0],[0,0]],[4,[-2,Color.YELLOW],[0,0],[0,0],[0,0]])] = True
         self.suicideDic[Action([3,[Color.YELLOW, -2],[-2,-2],[-2,-2],[0,0]],[4,[-2,Color.YELLOW],[0,0],[0,0],[0,0]])] = True
-        self.suicideDic[Action([3,[Color.YELLOW, -2],[-2,-2],[0,0],[0,0]],[4,[-2,-2],[-2,Color.RED],[0,0],[0,0]])] = True
+        self.suicideDic[Action([3,[Color.YELLOW, -2],[-2,-2],[0,0],[0,0]],[4,[-2,-2],[-2,Color.YELLOW],[0,0],[0,0]])] = True
         # end of pattern sandwich
-        # pattern deplacement obligatoire.
         
         #pattern make a 4-stack with a Yellow color on top, on an arrow case
         
         self.mustDoDic[Action([3,[-2,Color.YELLOW],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[-2,-2],[0,0]])] = True
-        self.mustDoDic[Action([3,[-2,-2],[-2,Color.YELLOW],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[0,0],[0,0]])] = True
-        self.mustDoDic[Action([3,[-2,-2],[-2,-2],[-2,Color.YELLOW],[0,0]],[4,[-2,-2],[0,0],[0,0],[0,0]])] = True
+        #self.mustDoDic[Action([3,[-2,-2],[-2,Color.YELLOW],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[0,0],[0,0]])] = True
+        #self.mustDoDic[Action([3,[-2,-2],[-2,-2],[-2,Color.YELLOW],[0,0]],[4,[-2,-2],[0,0],[0,0],[0,0]])] = True
         
         self.suicideDic[Action([3,[-2,Color.RED],[0,0],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[-2,-2],[0,0]])] = True
-        self.suicideDic[Action([3,[-2,-2],[-2,Color.RED],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[0,0],[0,0]])] = True
-        self.suicideDic[Action([3,[-2,-2],[-2,-2],[-2,Color.RED],[0,0]],[4,[-2,-2],[0,0],[0,0],[0,0]])] = True
+        #self.suicideDic[Action([3,[-2,-2],[-2,Color.RED],[0,0],[0,0]],[4,[-2,-2],[-2,-2],[0,0],[0,0]])] = True
+        #self.suicideDic[Action([3,[-2,-2],[-2,-2],[-2,Color.RED],[0,0]],[4,[-2,-2],[0,0],[0,0],[0,0]])] = True
     
         #pattern make a 4-stack with yellow color on bottom, on a blank case
         self.mustDoDic[Action([4,[-2,-2],[0,0],[0,0],[0,0]],[3,[Color.YELLOW,-2],[-2,-2],[-2,-2],[0,0]])] = True
-        self.mustDoDic[Action([4,[-2,-2],[-2,-2],[0,0],[0,0]],[3,[Color.YELLOW,-2],[-2,-2][0,0][0,0]])] = True
-        self.mustDoDic[Action([4,[-2,-2],[-2,-2],[-2,-2],[0,0]],[3,[Color.YELLOW,-2],[0,0][0,0][0,0]])] = True
+        #self.mustDoDic[Action([4,[-2,-2],[-2,-2],[0,0],[0,0]],[3,[Color.YELLOW,-2],[-2,-2],[0,0],[0,0]])] = True
+        #self.mustDoDic[Action([4,[-2,-2],[-2,-2],[-2,-2],[0,0]],[3,[Color.YELLOW,-2],[0,0],[0,0],[0,0]])] = True
         
         self.suicideDic[Action([4,[-2,-2],[0,0],[0,0],[0,0]],[3,[Color.RED,-2],[-2,-2],[-2,-2],[0,0]])] = True
-        self.suicideDic[Action([4,[-2,-2],[-2,-2],[0,0],[0,0]],[3,[Color.RED,-2],[-2,-2][0,0][0,0]])] = True
-        self.suicideDic[Action([4,[-2,-2],[-2,-2],[-2,-2],[0,0]],[3,[Color.RED,-2],[0,0][0,0][0,0]])] = True
+        #self.suicideDic[Action([4,[-2,-2],[-2,-2],[0,0],[0,0]],[3,[Color.RED,-2],[-2,-2],[0,0],[0,0]])] = True
+        #self.suicideDic[Action([4,[-2,-2],[-2,-2],[-2,-2],[0,0]],[3,[Color.RED,-2],[0,0],[0,0],[0,0]])] = True
         
         # broke an enemy tower. will be reversed after.
         self.mustDoDic[Action([4,[-2,Color.RED],[0,0],[0,0],[0,0]],[3,[2,-2],[-2,-2],[-2,-2],[0,0]])] = True
-        self.mustDoDic[Action([4,[-2,-2],[-2,Color.RED],[0,0],[0,0]],[3,[2,-2],[-2,-2],[0,0],[0,0]])] = True
-        self.mustDoDic[Action([4,[-2,-2],[-2,-2],[-2,Color.RED],[0,0]],[3,[2,-2],[0,0],[0,0],[0,0]])] = True
+        #self.mustDoDic[Action([4,[-2,-2],[-2,Color.RED],[0,0],[0,0]],[3,[2,-2],[-2,-2],[0,0],[0,0]])] = True
+        #self.mustDoDic[Action([4,[-2,-2],[-2,-2],[-2,Color.RED],[0,0]],[3,[2,-2],[0,0],[0,0],[0,0]])] = True
        
         # broke one of our tower.
         self.suicideDic[Action([4,[-2,Color.YELLOW],[0,0],[0,0],[0,0]],[3,[2,-2],[-2,-2],[-2,-2],[0,0]])] = True
-        self.suicideDic[Action([4,[-2,-2],[-2,Color.YELLOW],[0,0],[0,0]],[3,[2,-2],[-2,-2],[0,0],[0,0]])] = True
-        self.suicideDic[Action([4,[-2,-2],[-2,-2],[-2,Color.YELLOW],[0,0]],[3,[2,-2],[0,0],[0,0],[0,0]])] = True
+        #self.suicideDic[Action([4,[-2,-2],[-2,Color.YELLOW],[0,0],[0,0]],[3,[2,-2],[-2,-2],[0,0],[0,0]])] = True
+        #self.suicideDic[Action([4,[-2,-2],[-2,-2],[-2,Color.YELLOW],[0,0]],[3,[2,-2],[0,0],[0,0],[0,0]])] = True
+
+
+if __name__ == "__main__" :
+    player_main(Marvin_player())
--- milestone3/sarena/src/pattern.py
+++ milestone3/sarena/src/pattern.py
@@ -4,8 +4,8 @@ Created on 8 nov. 2012
 @author: Florentin
 '''
 class Color :
-    Color.YELLOW = 1
-    Color.RED = -1
+    YELLOW = 1
+    RED = -1
 
 
 class Action(object):
@@ -35,10 +35,12 @@ class Action(object):
         self.size_t_init = len(self.t_init)
         self.size_t_target = len(self.t_target)
         self.kwords = kwords
-        self.t_init.pop(0)
-        self.final_tower = self.t_target.append(self.t_init) 
         self.weight = self._compute_weight()
-        self.representation = hash(self.final_tower.append(self.weight))
+        self.t_target.extend(self.t_init) 
+        self.t_target.append(self.weight)
+        #print(self.t_target)
+        self.representation = self._make_representation()
+        #print(str(self.representation)+" tour final :"+str(self.t_target))
         if 'sub_board' in kwords :
             pass # analyse a pattern
         # define the representation TODO
@@ -49,45 +51,111 @@ class Action(object):
     def __hash__(self):
         return self.representation
     
+    def _make_representation(self):
+        return hash(str(self.t_target))
+    def _weight(self):
+        if self.size_t_init == 2 and self.size_t_target == 2 : # one coin each towers
+            self.weight = 1
+        elif self.size_t_init == 3 and self.size_t_target == 3 : # two coins on each towers
+            self.weight = 3
+        elif self.size_t_init == 2 and self.size_t_target == 3 :
+            self.weight = 2
+        elif self.size_t_init == 3 and self.size_t_target == 2 :
+            self.weight = 2 
+        elif self.size_t_init == 4 and self.size_t_target == 2 :
+            self.weight = 3
+        elif self.size_t_init == 2 and self.size_t_target == 4 :
+            self.weight = 3        
     def _detect_sandwich(self):
-            if (self.t_init[0][0] == Color.RED and self.t_target[self.size_t_target-1][1] == Color.RED) \
-                or (self.t_init[0][0] == Color.YELLOW and self.t_target[self.size_t_target-1][1] == Color.YELLOW):
-                # sandwich detected. Del unused color
-                i = 0
+            if self.size_t_init > 1 and self.size_t_target > 1:
+                if (self.t_init[1][0] == Color.RED and self.t_target[self.size_t_target-1][1] == Color.RED) \
+                    or (self.t_init[1][0] == Color.YELLOW and self.t_target[self.size_t_target-1][1] == Color.YELLOW):
+                    i = 1
+                    while i < self.size_t_init :
+                        if i == 1:
+                            self.t_init[i][1] = -2
+                        else :
+                            self.t_init[i][0] = -2
+                            self.t_init[i][1] = -2
+                        i+=1
+                    i = 1
+                    while i < self.size_t_target :
+                        if i == self.size_t_target-1 :
+                            self.t_target[i][0] = -2
+                        else :
+                            self.t_target[i][0] = -2
+                            self.t_target[i][1] = -2
+                        i+=1 
+                    self._weight()
+                    if self.t_init[1][0] == Color.YELLOW :
+                        self.weight = -self.weight
+                    self.t_target.pop(0)
+                    self.t_init.pop(0)
+                    return True
+                else :
+                    return False
+            else :
+                return False
+            
+    
+    def _detect_color_up(self):
+        if self.size_t_init + self.size_t_target == 6 and self.size_t_target >1 :
+            if self.t_init[self.size_t_init-1][1] == Color.YELLOW or self.t_init[self.size_t_init-1][1] == Color.RED :
+                
+                i = 1
                 while i < self.size_t_init :
-                    if i == 0 :
-                        self.t_init[i][1] = -2
+                    if i == self.size_t_init-1:
+                        self.t_init[i][0] = -2
                     else :
                         self.t_init[i][0] = -2
                         self.t_init[i][1] = -2
                     i+=1
                 i = 1
                 while i < self.size_t_target :
-                    if i == self.size_t_target-1 :
-                        self.t_target[i][0] = -2
+                    self.t_target[i][0] = -2
+                    self.t_target[i][1] = -2
+                    i+=1 
+                    
+                self._weight()
+                
+                if self.t_init[self.size_t_init-1][1] == Color.RED and self.t_target[0] == 4 :
+                    self.weight = -self.weight
+                elif self.t_init[self.size_t_init-1][1] == Color.YELLOW and self.t_target[0] == 3 and \
+                    self.t_target[1][0] != Color.YELLOW :
+                    self.t_target[1][0] = 2
+                    #choisir pattern
+                    self.weight = -self.weight
+                elif self.t_init[self.size_t_init-1][1] == Color.RED and self.t_target[0] == 3 and \
+                    self.t_target[1][0] != Color.YELLOW :
+                    self.weight = -self.weight
+                    self.t_target[1][0] = 2
+                self.t_init.pop(0)
+                return True
+        return False
+    def _detect_color_bot(self):
+        if self.size_t_init + self.size_t_target == 6 and self.size_t_target > 1 :
+            if (self.t_target[1][0] == Color.YELLOW or self.t_target[1][0] == Color.RED) and self.t_target[0] == 3 :
+                i = 1
+                while i < self.size_t_init :
+                        self.t_init[i][0] = -2
+                        self.t_init[i][1] = -2
+                        i+=1 
+                i = 1
+                while i < self.size_t_target :
+                    if i == 1 :
+                        self.t_target[i][1] = -2
                     else :
                         self.t_target[i][0] = -2
                         self.t_target[i][1] = -2
-                    i+=1 
-                if self.size_t_init == 2 and self.size_t_target == 2 : # one coin each towers
-                    self.weight = 1
-                elif self.size_t_init == 3 and self.size_t_target == 3 : # two coins on each towers
-                    self.weight = 3
-                elif self.size_t_init == 2 and self.size_t_target == 3 :
-                    self.weight = 2
-                elif self.size_t_init == 3 and self.size_t_target == 2 :
-                    self.weight = 2 
-                elif self.size_t_init == 4 and self.size_t_target == 2 :
-                    self.weight = 3
-                elif self.size_t_init == 2 and self.size_t_target == 4 :
-                    self.weight = 3
-                if self.t_init[0][0] == Color.YELLOW :
+                    i+=1
+                self._weight()
+                
+                if self.t_target[1][0] == Color.RED :
                     self.weight = -self.weight
-                self.t_target.pop(0)
+                self.t_init.pop(0)
                 return True
-            else :
-                return False
-             
+        return False
+        
     def _compute_weight(self):
         self.weight = 0
            
@@ -96,11 +164,15 @@ class Action(object):
         #basic action
         else :
             #detect sandwich
-            if self._detect_sandwich() :
+
+            #if self._detect_sandwich() :
+                #return self.weight
+            #if self._detect_color_up() :
+                #return self.weight
+            if self._detect_color_bot() :
                 return self.weight
-            else :
-                pass
+            else: return 0
                     
     def is_a_pattern(self,dico):
-        return dico[self.representation]
+        return self in dico
         
