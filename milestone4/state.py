4c5d41c4bea93557e3c1097e59d93779ff7c8784
Florentin Rochet <oversnals@hotmail.com>
2012-11-27 15:36:53 +0100

    state

---
+++ milestone4/state.py
@@ -0,0 +1,17 @@
+'''
+Created on 27 nov. 2012
+
+@author: Florentin
+'''
+
+class State(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
