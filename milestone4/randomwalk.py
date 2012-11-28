c46a3a2d4c3fef744f6c37e242da9c5a21bb7582
Florentin Rochet <oversnals@hotmail.com>
2012-11-28 18:08:16 +0100

    randomwalk

--- milestone4/randomwalk.py
+++ milestone4/randomwalk.py
@@ -5,16 +5,35 @@ Created on 27 nov. 2012
 '''
 
 from search import *
+from state import *
 
 class StupidTravelingSalesman(Problem):
     
     def __init__(self, initial, goal=None):
         self.closed = dict()
-        state_init = State(initial)
-        
-        self.closed[state_init] = True
-    
+        self.state_init = State(initial)
+        self.closed[self.state_init] = True
     def successor(self, state):
-        pass
+        list = self.state_init.swap()
+        while list :
+            newState = State(list)
+            if newState not in self.closed :
+                self.closed[newState] = True
+                yield (None, newState)
+                
+            
+    
+    def value(self,state):
+        """Compute the path value"""
+        return state.comput_path()
+    
+    
+if __name__ == "__main__":
+    
+    initial = [] # to be found with greedy method, léowlo-lo
+    
+    salesman = StupidTravelingSalesman(initial)
+    result = random_walk(salesman)
+    print(result)
     
     
--- milestone4/state.py
+++ milestone4/state.py
@@ -10,8 +10,21 @@ class State(object):
     '''
 
 
-    def __init__(self):
+    def __init__(self, vertices):
         '''
         Constructor
         '''
+        self.vertices = vertices
+        self.nbr_of_swap = 0
+        
+    def swap(self):
+        """
+            do a swap, return a new vertices. 
+            return False if all the swap have been done
+        """
+        pass
+    
+    def compute_path(self):
+        pass
+        
         
