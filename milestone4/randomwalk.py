085c4fd675881b79b19844b590dae521a8946833
Florentin Rochet <oversnals@hotmail.com>
2012-11-28 19:25:34 +0100

    simplification du code

--- milestone4/randomwalk.py
+++ milestone4/randomwalk.py
@@ -16,9 +16,6 @@ class TravelingSalesman(Problem):
         
     def successor(self, state):
         yield (None, State(self.state_init.swap()))
-                
-            
-    
     def value(self,state):
         """Compute the path value"""
         return state.compute_path()
