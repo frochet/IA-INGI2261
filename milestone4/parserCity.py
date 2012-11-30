15a4458f9ecdc1461a5eeb1d2dba1f895ab3cc35
snals <florentin.rochet@gmail.com>
2012-11-30 13:40:12 +0100

    ajotu de parserCity

---
+++ milestone4/parserCity.py
@@ -0,0 +1,36 @@
+'''
+Created on 27 nov. 2012
+
+@author: Florentin
+'''
+
+from IO import *
+
+class Parser(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,filename):
+        
+        '''
+        Constructor
+        '''
+        self.io = IO(filename);
+    
+    def parse_line(self,separator = " "):
+        
+        matrice = []
+        self.io.init_reader()
+        for line in self.io.file :
+            if separator != None :
+                line = line.replace("\n","")
+                tab = line.split(separator)
+                i = 0
+                for elem in tab :
+                    tab[i] = int(elem)
+                    i+=1
+                matrice.append(tab)
+        return matrice
+        
