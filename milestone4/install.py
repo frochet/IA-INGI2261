7955eb06ee3c6c4a95586c7d6e61341721f2a608
Florentin Rochet <oversnals@hotmail.com>
2012-12-12 03:02:19 +0100

    boup

--- milestone4/install.py
+++ milestone4/install.py
@@ -5,6 +5,7 @@ Created on Dec 11, 2012
 '''
 from packages import *
 import sys
+from minisat import *
 
 def depends(A, B):
     return (-A, B)
@@ -13,25 +14,36 @@ def conflicts(A, B):
 def provides(A, B):
     return (-A, B)
 
+def look_if_is_provided_package(item,variables,clauses,rep):
+    for rootPackage in rep.packages :
+        for provided in rootPackage.provides:
+            if item == provided :
+                get_clauses(rep, [(rootPackage,)],variables,clauses)
+                clauses += [provides(variables.index(rootPackage)+1, variables.index(item)+1)]
 
-def get_clauses(toinstall, full_list = []):
-    clauses = []
-    variables = []
+def get_clauses(rep,toinstall, full_list = [], full_clause = []):
+    clauses = full_clause
+    variables = full_list
     for package in toinstall:
         for pckg in package:
-            if pckg not in variables + full_list :
+            if pckg not in variables :
                 variables += [pckg]
-                (var, cl) = get_clauses(pckg.depends, variables)
-                for item in var:
-                    if item not in variables + full_list:
-                        variables += [item]
-                        clauses += [depends(variables.index(pckg)+1, variables.index(item)+1)]
+                (var, cl) = get_clauses(rep,pckg.depends, variables,clauses)
+                for tuple in pckg.depends :
+                    for item in tuple :
+                        clauses += [depends(variables.index(pckg)+1, variables.index(item)+1)]                
+                        # +  look if item is a provided package
+                        look_if_is_provided_package(item,variables,clauses,rep)
+                        ##() = get_clauses(rep,packageToAddToInstallation,variables)
+                        #toinstall+=[(packageToAddToInstallation,)]
+                        #clauses += [provides(variables.index(item)+1,variables.index(packageToAddToInstallation)+1)]
                 for conflict in pckg.conflicts:
-                    if conflict in variables + full_list :
+                    if conflict in variables :
                         clauses += [conflicts(variables.index(pckg)+1, variables.index(conflict)+1)]
                     else:
                         variables += [conflict]
                         clauses += [conflicts(variables.index(pckg)+1, variables.index(conflict)+1)]
+               
     return (variables, clauses)
 
             
@@ -43,5 +55,6 @@ if __name__ == "__main__":
     tupletoinstall = []
     for pck in toinstall:
         tupletoinstall += [(rep[pck],)]
-    (n, clauses) = get_clauses(tupletoinstall)
+    (n, clauses) = get_clauses(rep,tupletoinstall)
+    
     print((n,clauses))
