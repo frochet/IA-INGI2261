d76f869953b3144856965f36e5b427d095e40afe
Leonard Debroux <leonard.debroux@gmail.com>
2012-12-12 10:54:43 +0100

    changes to deal with or dependencies

--- milestone4/install.py
+++ milestone4/install.py
@@ -9,6 +9,13 @@ from minisat import *
 
 def depends(A, B):
     return (-A, B)
+def depends_on_or(A, B):
+    ''' B is a list of indexes of or dependencies for A
+    '''
+    result = (-A, )
+    for pckg in B:
+        result += (B, 0)
+    return result
 def conflicts(A, B):
     return (-A, -B)
 def provides(A, B):
@@ -30,13 +37,26 @@ def get_clauses(rep,toinstall, full_list = [], full_clause = []):
                 variables += [pckg]
                 (var, cl) = get_clauses(rep,pckg.depends, variables,clauses)
                 for tuple in pckg.depends :
-                    for item in tuple :
-                        clauses += [depends(variables.index(pckg)+1, variables.index(item)+1)]                
-                        # +  look if item is a provided package
-                        look_if_is_provided_package(item,variables,clauses,rep)
-                        ##() = get_clauses(rep,packageToAddToInstallation,variables)
-                        #toinstall+=[(packageToAddToInstallation,)]
-                        #clauses += [provides(variables.index(item)+1,variables.index(packageToAddToInstallation)+1)]
+                    if(len(tuple) == 1):
+                        if tuple[0] not in variables:
+                            variables += [tuple[0]]
+                        clauses += [depends(variables.index(pckg)+1, variables.index(tuple[0])+1)] 
+                        look_if_is_provided_package(tuple[0],variables,clauses,rep)
+                        #for item in tuple :
+                            #clauses += [depends(variables.index(pckg)+1, variables.index(item)+1)]                
+                            # +  look if item is a provided package
+                            #look_if_is_provided_package(item,variables,clauses,rep)
+                            ##() = get_clauses(rep,packageToAddToInstallation,variables)
+                            #toinstall+=[(packageToAddToInstallation,)]
+                            #clauses += [provides(variables.index(item)+1,variables.index(packageToAddToInstallation)+1)]
+                    else:
+                        indexes = []
+                        for item in tuple:
+                            if item not in variables:
+                                variables += item
+                            indexes += variables.index(item)+1
+                        clauses += [depends_on_or(variables.index(pckg)+1, indexes)]
+                        
                 for conflict in pckg.conflicts:
                     if conflict in variables :
                         clauses += [conflicts(variables.index(pckg)+1, variables.index(conflict)+1)]
