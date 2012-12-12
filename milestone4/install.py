ac20c11273db916c7c63f0895abcf569724673f8
Florentin Rochet <oversnals@hotmail.com>
2012-12-12 01:48:15 +0100

    comprends les conflicts

--- milestone4/install.py
+++ milestone4/install.py
@@ -14,19 +14,24 @@ def provides(A, B):
     return (-A, B)
 
 
-def get_clauses(rep, toinstall, full_list = []):
+def get_clauses(toinstall, full_list = []):
     clauses = []
     variables = []
     for package in toinstall:
         for pckg in package:
-            variables += [pckg]
-            (var, cl) = get_clauses(rep, pckg.depends, variables)
-            for item in var:
-                variables += item
-                clauses += [depends(pckg, item)]
-            for conflict in pckg.conflicts:
-                if conflict in full_list + variables:
-                    clauses += conflicts(pckg, conflict)
+            if pckg not in variables + full_list :
+                variables += [pckg]
+                (var, cl) = get_clauses(pckg.depends, variables)
+                for item in var:
+                    if item not in variables + full_list:
+                        variables += [item]
+                        clauses += [depends(variables.index(pckg)+1, variables.index(item)+1)]
+                for conflict in pckg.conflicts:
+                    if conflict in variables + full_list :
+                        clauses += [conflicts(variables.index(pckg)+1, variables.index(conflict)+1)]
+                    else:
+                        variables += [conflict]
+                        clauses += [conflicts(variables.index(pckg)+1, variables.index(conflict)+1)]
     return (variables, clauses)
 
             
@@ -37,5 +42,6 @@ if __name__ == "__main__":
     toinstall = sys.argv[2:]
     tupletoinstall = []
     for pck in toinstall:
-        tupletoinstall += [(pck)]
-    (n, clauses) = get_clauses(rep, toinstall)
+        tupletoinstall += [(rep[pck],)]
+    (n, clauses) = get_clauses(tupletoinstall)
+    print((n,clauses))
